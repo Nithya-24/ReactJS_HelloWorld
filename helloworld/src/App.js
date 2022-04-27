126fbb33cb43c19e349b5f110dc6d9eb26cbf761
Your Name <nithyaramadoz@gmail.com>
2022-04-27 22:04:12 +0530

    OnClick event is added

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import logo from './assets/images/logo.png';
 
 class App extends React.Component {
+  url="https://www.bridgelabz.com/"
 
   constructor() {
     super() 
@@ -11,11 +12,19 @@ class App extends React.Component {
     }
   }
 
+  /**
+   *  onclick Funciton
+   */
+  onClick = ($event) => {
+    window.open(this.url,"_blank");
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="BridgeLabz Logo goes here."/>
+        <img src={logo} onClick={this.onClick} 
+          alt="BridgeLabz Logo goes here."/>
       </div>
     )
   }
