68145286e9309709c04ca77ed5d20d326fadcf18
Your Name <nithyaramadoz@gmail.com>
2022-04-28 09:47:05 +0530

    Added: Completed the Hello from BL Page

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -33,19 +33,36 @@ class App extends React.Component {
   
     render() {
       return (
-        <>
-          <div>
-            <h1>Hello {this.state.userName} from BridgeLabz</h1>
-            <img src={logo} onClick={this.onClick} 
-              alt="BridgeLabz Logo goes here."/>
-          </div>
-          <div>
-            <h1><input onChange={this.onNameChange} /> </h1>
-            <h6><span className='error-output'>{this.state.nameError}</span></h6>
-          </div>
-        </>
-      )
-    }
-  } 
-  
-  export default App;
+        <div className="container">
+        <div className="main-container">
+        <div class="page-content">
+        <h1>Hello {this.state.userName} from BridgeLabz</h1>
+        <img src={logo} onClick={this.onClick} 
+            alt="BridgeLabz Logo goes here"/>
+        <div class="input-content">
+          <input onChange={this.onNameChange} />
+          <span className='error-output'>{this.state.nameError}</span>
+        </div>
+        <p>At BridgeLabz, we are a tech community of</p>
+        <ul>
+            <li>technologists</li>
+            <li>thinkers</li>
+            <li>builders</li>
+        </ul>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+            To know about us, visit <a href = {this.url} onClick={this.onClick}>BridgeLabz </a> 
+            to learn even more about our misiion i.e. <strong>Employability to all</strong>.
+        </p>  
+      </div>
+      </div>
+      </div>
+    )
+  }
+} 
+export default App;
---
+++ helloworld/src/Home.js
@@ -0,0 +1,14 @@
+import logo from './assets/images/logo.png';
+import React from 'react';
+import './App.css';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
+export default Home;
